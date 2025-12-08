6fba0f1c7a5a33eda84946d82843df3e903cd73e
Rasha92zee <rashahasoon923@gmail.com>
2025-12-09 00:00:08 +0530

    logo

--- berry-basket/src/Components/Header/index.js
+++ berry-basket/src/Components/Header/index.js
@@ -1,12 +1,29 @@
+import Link from 'react-router-dom';
+import Logo from '../../assets/images/logo.png';
+
 const Header = () => {
     return (
         <>
-            <div className="header">
-                <div className="top-strip .bg-blue">
+            <div className="headerWrapper">
+                <div className="top-strip bg-blue">
                     <div className="container">
-                        <p className="mb-0 mt-0 text-center">Free Shipping Over <b>$100</b> & Free Returns</p>
+                        <p className="mb-0 mt-0 text-center">Free Shipping Over <b>
+                            $100</b> & Free Returns</p>
                     </div>
                 </div>
+
+                <div className="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="logoWrapper col-sm-3">
+                                <Link to="/">
+                                    <img src={Logo} alt="Berry Basket Logo" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>  
         </>
     )
